bd4718b0e454220737e60aa92549575590f32ba5
Hermine Gh <Hgh_Wteach@yahoo.com>
2022-02-15 01:19:34 +0400

    async_file_read

--- application.js
+++ application.js
@@ -1,45 +1,51 @@
 //  modules
-
 const fs = require('fs');
 const path = require('path');
+const fsPromises = require('fs/promises');
 const sizeFormate = require('./src/components/modules/modules');
 
 // path from command line arguments
 let pathName = process.argv[2];
 
+// go up
 if (path.dirname(pathName) === '../..') {
-  //go up
   pathName = path.dirname(pathName).split(path.sep).pop();
 }
-const filesArr = (comLineArg, data) => {
-  // array for all files
+
+const filesArr = async (argvData, data) => {
   const allFiles = data || [];
   try {
-    fs.readdirSync(comLineArg).forEach((cur) => {
-      const curPath = path.join(comLineArg, cur);
-      if (fs.statSync(curPath).isFile()) {
-        const { size } = fs.statSync(curPath);
-        allFiles.push([size, sizeFormate(curPath, size)]);
-      } else {
-        filesArr(curPath, allFiles);
-      }
-    });
+    const paths = await fsPromises.readdir(argvData);
+    await Promise.all(
+      paths.map(async (cur) => {
+        const curPath = path.join(argvData, cur);
+        const state = await fsPromises.stat(curPath);
+
+        if (state.isFile()) {
+          const { size } = state;
+          allFiles.push([size, sizeFormate(curPath, size)]);
+        } else {
+          return filesArr(curPath, allFiles);
+        }
+        return allFiles;
+      })
+    );
   } catch (err) {
-    console.log('ERROR : no access permition', err.message);
+    console.log(err.message);
   }
   return allFiles.sort((a, b) => b[0] - a[0]);
 };
 
-const writeToFile = (infoArr) => {
+const writeToFile = async () => {
   try {
-    fs.writeFileSync('./sorted_files.txt', 'sorted_files.txt\n\n\n');
-    infoArr.forEach((el) => {
-      fs.appendFileSync('./sorted_files.txt', el[1].toString());
-    });
+    const files = await filesArr(pathName);
+    await fsPromises.writeFile('./sorted_files.txt', 'sorted_files.txt\n\n\n');
+    files.forEach((el) =>
+      fs.appendFileSync('sorted_files.txt', el[1].toString())
+    );
   } catch (err) {
-    console.log('ERROR ❗ There are no files in the specified directory');
+    console.log(err.message);
   }
 };
 
-const sortedData = filesArr(pathName);
-writeToFile(sortedData);
+writeToFile();
